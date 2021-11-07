c250075457fe4fda7c86e1f2238ab3056d3345ed
robinson <robinjosellove@gmail.com>
2021-11-06 21:52:56 -0500

    changes

--- src/components/list.jsx
+++ src/components/list.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import Items from "./form_items";
+import axios from 'axios';
 import FormUpdate from './form_update';
 
 const List = ({ id, cantidad, producto, precio_unit, precio_tot }) => {
@@ -13,6 +13,8 @@ const List = ({ id, cantidad, producto, precio_unit, precio_tot }) => {
 
   });
 
+  
+
   const handleId = (id) => {
     setUpdateId({
       id: id,
@@ -22,6 +24,19 @@ const List = ({ id, cantidad, producto, precio_unit, precio_tot }) => {
       precio_tot: precio_tot
     });
     
+  }
+
+  const handleDeleteVenta = (id) => {
+    
+    const headers = {
+      "Access-Control-Request-Method": "DELETE"
+        };
+
+        axios.delete(`http://localhost/apidistribuidas/controller/venta.php?opc=DeleteP/`, id).then(function(response) {
+            console.log(response);
+        }).catch(function(error) {
+            console.log(error);
+        })
   }
   
 
@@ -69,7 +84,7 @@ const List = ({ id, cantidad, producto, precio_unit, precio_tot }) => {
               </div>
             </div>
           <div className="col-md-3">            
-          <button type="button" className="btn btn-danger btn-lg" data-bs-toggle="modal" data-bs-target="#eliminar">
+          <button type="button"  className="btn btn-danger btn-lg" data-bs-toggle="modal" data-bs-target="#eliminar">
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
   <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
   <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
@@ -92,7 +107,7 @@ const List = ({ id, cantidad, producto, precio_unit, precio_tot }) => {
                       <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
                       </svg> Cerrar
                     </button>
-                    <button type="button" className="btn btn-success">
+                    <button type="button" onClick={() => handleDeleteVenta(id)} className="btn btn-success">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
   <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
   <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
